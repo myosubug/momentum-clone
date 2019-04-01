fae9fc1a208fd7630b4bd89b36f22789c93c8ae3
myosubug <seanoh.dev@gmail.com>
2019-03-31 22:16:54 -0600

    creating todo list

---
+++ todo.js
@@ -0,0 +1,38 @@
+const todoForm = document.querySelector(".js-todoForm");
+const todoInput = todoForm.querySelector("input");
+const todoList = document.querySelector(".js-todoList");
+
+const TODO_LS = 'toDos';
+
+function paintTodo(text){
+     const li = document.createElement("li");
+     const delBtn = document.createElement("button");
+     delBtn.innerText = "❌";
+     const span = document.createElement("span");
+     span.innerText = text;
+     li.appendChild(span);
+     li.appendChild(delBtn);
+     todoList.appendChild(li);
+}
+
+function handleSubmit(event){
+    event.preventDefault();
+    const currentValue = todoInput.value;
+    paintTodo(currentValue);
+    todoInput.value = "";
+}
+
+
+function loadTodo(){
+    const TODOS = localStorage.getItem(TODO_LS);
+    if(TODOS !== null){
+
+    } 
+}
+
+function init(){
+    loadTodo();
+    todoForm.addEventListener("submit", handleSubmit);
+}
+
+init();
