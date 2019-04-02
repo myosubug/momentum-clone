9ab9588a4eb4c9b0bead3764ce748ba9d45533a9
myosubug <seanoh.dev@gmail.com>
2019-04-02 00:11:56 -0600

    now todo intputs are saved in local storage

--- todo.js
+++ todo.js
@@ -2,17 +2,30 @@ const todoForm = document.querySelector(".js-todoForm");
 const todoInput = todoForm.querySelector("input");
 const todoList = document.querySelector(".js-todoList");
 
-const TODO_LS = 'toDos';
+const TODO_LS = "toDos";
+const todos = [];
+
+
+function savTodos() {
+    localStorage.setItem(TODO_LS, JSON.stringify(todos));
+}
 
 function paintTodo(text){
      const li = document.createElement("li");
      const delBtn = document.createElement("button");
-     delBtn.innerText = "❌";
      const span = document.createElement("span");
+     const newID = todos.length + 1;
+     const todoObj = {
+        text: text,
+        id: newID
+    };
+     delBtn.innerText = "❌";
      span.innerText = text;
      li.appendChild(delBtn);
      li.appendChild(span);
      todoList.appendChild(li);
+     todos.push(todoObj);
+     savTodos();
 }
 
 function handleSubmit(event){
@@ -24,9 +37,12 @@ function handleSubmit(event){
 
 
 function loadTodo(){
-    const TODOS = localStorage.getItem(TODO_LS);
-    if(TODOS !== null){
-
+    const loadedTodos = localStorage.getItem(TODO_LS);
+    if(loadedTodos !== null){
+        const parsedTodos = JSON.parse(loadedTodos);
+        parsedTodos.forEach(function(todo){
+            paintTodo(todo.text);
+        });
     } 
 }
 
